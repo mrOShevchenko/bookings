34c4c8761c70bf129657ef1a2b038c4ce443c222
Oleksii Shevchenko <a380987379715@gmail.com>
2023-10-14 14:31:25 +0200

    done 2.8.7 (validation I part)

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"bookings/internal/config"
+	"bookings/internal/forms"
 	"bookings/internal/models"
 	"bookings/internal/render"
 	"encoding/json"
@@ -55,7 +56,15 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+
+}
+
+// PostReservation handles the reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
 }
 
 // Generals renders the room page
